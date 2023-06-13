b8c1f7095c5114077dc861e022d11b673d6d8a1e
Incutti <mateincu@gmail.com>
2023-06-13 16:00:44 -0300

    TERMINAMOS LA BENDITA PAGINAAAA

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = 3000
 const path=require("path");
 const images=path.join(__dirname,"images");
 app.use(express.static(images));
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/formulario', (req, res) => {
   res.sendFile(__dirname + '/paginaLibros.html')
@@ -16,7 +19,7 @@ app.get('/carrusel', (req, res) => {
 
 
 app.post('/insertar', (req, res) => {
-  console.log(req)
+  console.log(req.body)
   var mysql = require('mysql');
   var conexion= mysql.createConnection({
     host : 'localhost',
@@ -32,9 +35,16 @@ app.post('/insertar', (req, res) => {
     console.log('Conectado con el identificador ' + conexion.threadId);
   });
 
-  const{email,contrasenia}= req.body;
-  const sql = 'insert into mensaje values(NULL,"'+ email+'","'+contrasenia+'")';
-  const values=[email,contrasenia];
+  const{email,mensaje}= req.body;
+  const sql = 'insert into mensaje values(NULL,"'+email+'","'+mensaje+'")';
+  const values=[email,mensaje];
+
+  conexion.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+    });
+  
+
 
 })
 
